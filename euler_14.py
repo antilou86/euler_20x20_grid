7000505986e804ef28c121ce104c3356acf5ead8
luis guzman <antilou86@gmail.com>
2020-02-24 19:58:25 -0500

    completed, runs in about 30s

--- euler_14.py
+++ euler_14.py
@@ -10,10 +10,37 @@
 
 # Which starting number, under one million, produces the longest chain?
 # NOTE: Once the chain starts the terms are allowed to go above one million.
-limit = 1000000
-def longest_chain(n):
-    pass
+import time
 
+limit = 1000000
+
+
+def longest_chain(n):
+    t = time.time()
+    longest_start = 0
+    longest_chain = 0
+    
+
+    for i in range(n):
+        chain_length = 0
+        num = i
+
+        while num > 1:
+
+            if num%2==0: #if even
+                num = num/2
+                chain_length+=1
+            else: #if odd
+                num = (3*num)+1
+                chain_length+=1
+
+        if chain_length > longest_chain:
+            longest_chain = chain_length
+            longest_start = i
+            
+    tt = time.time() - t
+    print(f"ran in {tt:.5f}s \n")
+    return longest_start
 
 
 print(f"{longest_chain(limit)}")
